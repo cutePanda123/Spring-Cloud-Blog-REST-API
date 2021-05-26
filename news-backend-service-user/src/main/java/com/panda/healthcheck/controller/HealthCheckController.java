e15651de0f9c14ed74b87912210e2d9e19ca2039
cutePanda123 <peterli1234@126.com>
2021-05-25 22:33:38 -0700

    setup logback log config

--- news-backend-service-user/src/main/java/com/panda/healthcheck/controller/HealthCheckController.java
+++ news-backend-service-user/src/main/java/com/panda/healthcheck/controller/HealthCheckController.java
@@ -1,11 +1,17 @@
 package com.panda.healthcheck.controller;
 
 import com.panda.api.controller.healthcheck.HealthCheckControllerApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController implements HealthCheckControllerApi {
+    final static Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+
     public Object healthcheck() {
+
+        logger.info("health check request received.");
         return "Health check passed.";
     }
 }
