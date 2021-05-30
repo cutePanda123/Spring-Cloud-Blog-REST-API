8c14b0841d3903002e4ada055439fddf32c61ffe
cutePanda123 <peterli1234@126.com>
2021-05-29 17:20:49 -0700

    integrate generated db models

---
+++ news-backend-service-user/src/main/java/com/panda/user/mapper/AppUserMapper.java
@@ -0,0 +1,7 @@
+package com.panda.user.mapper;
+
+import com.panda.my.mapper.MyMapper;
+import com.panda.pojo.AppUser;
+
+public interface AppUserMapper extends MyMapper<AppUser> {
+}
---
+++ news-backend-service-user/src/main/java/com/panda/user/mapper/FansMapper.java
@@ -0,0 +1,7 @@
+package com.panda.user.mapper;
+
+import com.panda.my.mapper.MyMapper;
+import com.panda.pojo.Fans;
+
+public interface FansMapper extends MyMapper<Fans> {
+}
