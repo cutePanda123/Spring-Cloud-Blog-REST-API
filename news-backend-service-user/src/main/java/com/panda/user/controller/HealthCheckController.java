f75dedd78b4fd756f1783b042a7104378c4646e8
cutePanda123 <peterli1234@126.com>
2021-05-31 21:27:36 -0700

    setup swagger api doc tool

--- news-backend-service-user/src/main/java/com/panda/user/controller/HealthCheckController.java
+++ news-backend-service-user/src/main/java/com/panda/user/controller/HealthCheckController.java
@@ -1,15 +1,19 @@
-package com.panda.healthcheck.controller;
+package com.panda.user.controller;
 
 import com.panda.api.controller.healthcheck.HealthCheckControllerApi;
 import com.panda.json.result.ResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "User health check controller", tags = {"health check"})
 public class HealthCheckController implements HealthCheckControllerApi {
     final static Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
+    @ApiOperation(value = "health check api", notes = "health chec api", httpMethod = "GET")
     public Object healthcheck() {
         logger.info("health check request received.");
         return ResponseResult.ok("Health check passed.");
