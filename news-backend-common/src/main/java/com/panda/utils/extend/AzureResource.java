e4d71c049070edae526caaa42d468cd7aaadab60
cutePanda123 <peterli1234@126.com>
2021-06-01 09:16:12 -0700

    added lombak package

--- news-backend-common/src/main/java/com/panda/utils/extend/AzureResource.java
+++ news-backend-common/src/main/java/com/panda/utils/extend/AzureResource.java
@@ -1,11 +1,14 @@
 package com.panda.utils.extend;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("classpath:azure.properties")
-@ConfigurationProperties(prefix = "azure")
+@ConfigurationProperties(prefix = "azure.communication_service")
+@Data
 public class AzureResource {
+    private String communicationServiceConnectionString;
 }
