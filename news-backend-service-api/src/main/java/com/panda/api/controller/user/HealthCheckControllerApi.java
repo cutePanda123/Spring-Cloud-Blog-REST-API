2b53a1354ab7abb052c045cf79325ea5ed19581f
cutePanda123 <peterli1234@126.com>
2021-06-04 06:03:01 -0700

    integrate redis to application

--- news-backend-service-api/src/main/java/com/panda/api/controller/user/HealthCheckControllerApi.java
+++ news-backend-service-api/src/main/java/com/panda/api/controller/user/HealthCheckControllerApi.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Api(value = "User health check controller", tags = {"health check"})
 public interface HealthCheckControllerApi {
-    @ApiOperation(value = "health check api", notes = "health chec api", httpMethod = "GET")
-    @GetMapping("/healthcheck")
-    Object healthcheck();
+    @ApiOperation(value = "server health check api", notes = "server health check api", httpMethod = "GET")
+    @GetMapping("/checkwebserver")
+    Object checkWebServer();
+
+    @ApiOperation(value = "redis health check api", notes = "redis health check api", httpMethod = "GET")
+    @GetMapping("/checkredis")
+    public Object checkRedis();
 }
--- news-backend-service-user/src/main/java/com/panda/user/controller/HealthCheckController.java
+++ news-backend-service-user/src/main/java/com/panda/user/controller/HealthCheckController.java
@@ -2,16 +2,26 @@ package com.panda.user.controller;
 
 import com.panda.api.controller.user.HealthCheckControllerApi;
 import com.panda.json.result.ResponseResult;
+import com.panda.utils.RedisAdaptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController implements HealthCheckControllerApi {
     final static Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
-    public Object healthcheck() {
+    @Autowired
+    private RedisAdaptor redisAdaptor;
+
+    public Object checkWebServer() {
         logger.info("health check request received.");
         return ResponseResult.ok("Health check passed.");
+    }
+
+    public Object checkRedis() {
+        redisAdaptor.set("abc", "redis is running well");
+        return ResponseResult.ok(redisAdaptor.get("abc"));
     }
 }
