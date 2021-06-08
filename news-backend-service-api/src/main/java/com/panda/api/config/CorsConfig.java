b3a89facab8816cddce7493564c65f68db8acacd
cutePanda123 <peterli1234@126.com>
2021-06-07 23:25:51 -0700

    fixed bug in get sms endpoint

---
+++ news-backend-service-api/src/main/java/com/panda/api/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.panda.api.config;
+
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@NoArgsConstructor
+public class CorsConfig {
+    @Bean
+    CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(corsConfigurationSource);
+    }
+}
--- news-backend-service-api/src/main/java/com/panda/api/controller/user/PassportControllerApi.java
+++ news-backend-service-api/src/main/java/com/panda/api/controller/user/PassportControllerApi.java
@@ -3,15 +3,16 @@ package com.panda.api.controller.user;
 import com.panda.json.result.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Api(value = "User login/register", tags = {"Login/Register controller"})
-@RequestMapping("passport")
+@RequestMapping("/api/service-user/passport")
 public interface PassportControllerApi {
     @ApiOperation(value = "Login/Register controller", notes = "Login/Register controller", httpMethod = "POST")
-    @PostMapping("/getSMSCode")
+    @GetMapping("/getSMSCode")
     public ResponseResult getSMSCode(String mobile, HttpServletRequest request);
 }
