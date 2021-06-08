281be1939ee08d6e535e8179c0d81305ea768811
cutePanda123 <peterli1234@126.com>
2021-06-07 23:26:07 -0700

    update url of health check endpoints

--- news-backend-service-api/src/main/java/com/panda/api/controller/user/HealthCheckControllerApi.java
+++ news-backend-service-api/src/main/java/com/panda/api/controller/user/HealthCheckControllerApi.java
@@ -3,8 +3,10 @@ package com.panda.api.controller.user;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Api(value = "User health check controller", tags = {"health check"})
+@RequestMapping("/api/service-user/healtch")
 public interface HealthCheckControllerApi {
     @ApiOperation(value = "server health check api", notes = "server health check api", httpMethod = "GET")
     @GetMapping("/checkwebserver")
