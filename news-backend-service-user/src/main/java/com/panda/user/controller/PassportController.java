4ccc49bdbd2b43cec30034703f548bee57b9815b
cutePanda123 <peterli1234@126.com>
2021-06-18 07:34:40 -0700

    added user account status check in user login controller

--- news-backend-service-user/src/main/java/com/panda/user/controller/PassportController.java
+++ news-backend-service-user/src/main/java/com/panda/user/controller/PassportController.java
@@ -2,8 +2,10 @@ package com.panda.user.controller;
 
 import com.panda.api.controller.BaseController;
 import com.panda.api.controller.user.PassportControllerApi;
+import com.panda.enums.UserAccountStatus;
 import com.panda.json.result.ResponseResult;
 import com.panda.json.result.ResponseStatusEnum;
+import com.panda.pojo.AppUser;
 import com.panda.pojo.bo.RegisterLoginBO;
 import com.panda.user.service.UserService;
 import com.panda.utils.IPUtils;
@@ -16,12 +18,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 public class PassportController extends BaseController implements PassportControllerApi {
@@ -37,13 +41,13 @@ public class PassportController extends BaseController implements PassportContro
     UserService userService;
 
     @Override
-    public ResponseResult getSMSCode(String mobile, HttpServletRequest request) {
+    public ResponseResult getSMSCode(@RequestParam String mobile, HttpServletRequest request) {
         String userIp = IPUtils.getRequestIp(request);
         redisAdaptor.setnx60s(MOBILE_SMSCODE + ":" + userIp, userIp);
 
-        String random = String.valueOf((Math.random() * 9 + 1) * 100000);
-        smsUtils.sendSmsMock("+12345678", random);
-        redisAdaptor.set(MOBILE_SMSCODE + ":" + mobile, random, 30 * 60);
+        int code = new Random().nextInt(100000) + 100000;
+        smsUtils.sendSmsMock(mobile, String.valueOf(code));
+        redisAdaptor.set(MOBILE_SMSCODE + ":" + mobile, String.valueOf(code), 30 * 60);
 
         return ResponseResult.ok();
     }
@@ -60,7 +64,13 @@ public class PassportController extends BaseController implements PassportContro
         if (StringUtils.isBlank(redisSmsCode) || !redisSmsCode.equals(smsCode)) {
             return ResponseResult.errorCustom(ResponseStatusEnum.SMS_CODE_ERROR);
         }
+        AppUser user = userService.queryMobileIsExist(mobile);
+        if (user != null && user.getActiveStatus() == UserAccountStatus.FROZEN.type) {
+            return ResponseResult.errorCustom(ResponseStatusEnum.USER_FROZEN);
+        } else if (user == null) {
+            user = userService.createUser(mobile);
+        }
 
-        return ResponseResult.ok();
+        return ResponseResult.ok(mobile);
     }
 }
