c0b099bc03e2d2dfb0d1b73fa40f96a853c167df
cutePanda123 <peterli1234@126.com>
2021-06-27 11:02:53 -0700

    fixed spring start issue to execlue data source auto config in file service startup

--- news-backend-service-fs/src/main/java/com/panda/fs/Application.java
+++ news-backend-service-fs/src/main/java/com/panda/fs/Application.java
@@ -2,10 +2,11 @@ package com.panda.fs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
