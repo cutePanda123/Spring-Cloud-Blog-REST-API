67a2735fb2ca1a546ca7285e8f9fd95886139025
cutePanda123 <peterli1234@126.com>
2021-06-27 11:03:14 -0700

    fixed file service health check url

--- news-backend-service-fs/src/main/java/com/panda/fs/controller/HealthCheckController.java
+++ news-backend-service-fs/src/main/java/com/panda/fs/controller/HealthCheckController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/service-user/healtcheck")
+@RequestMapping("/api/service-fs/healthcheck")
 public class HealthCheckController {
     final static Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
