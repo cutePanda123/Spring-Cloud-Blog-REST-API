f54af6200fa7b18ccb688009f8d837ba9fdd61b0
cutePanda123 <peterli1234@126.com>
2021-06-27 19:46:41 -0700

    added centralized max upload file size exception handler

--- news-backend-common/src/main/java/com/panda/exception/CustomExceptionHandler.java
+++ news-backend-common/src/main/java/com/panda/exception/CustomExceptionHandler.java
@@ -1,17 +1,25 @@
 package com.panda.exception;
 
 import com.panda.json.result.ResponseResult;
+import com.panda.json.result.ResponseStatusEnum;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
-
     @ExceptionHandler(CustomException.class)
     @ResponseBody
     public ResponseResult returnExceptionResult(CustomException e) {
         e.printStackTrace();
         return ResponseResult.exception(e.getResponseStatusEnum());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseBody
+    public ResponseResult returnMaxUploadSizeExceededExceptionResult(MaxUploadSizeExceededException e) {
+        e.printStackTrace();
+        return ResponseResult.exception(ResponseStatusEnum.FILE_MAX_SIZE_ERROR);
     }
 }
