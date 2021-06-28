f24be71c89168080874ea1a5c917f5d3ef1c5b78
cutePanda123 <peterli1234@126.com>
2021-06-27 19:40:22 -0700

    implement file resource class to get file server host url

---
+++ news-backend-service-fs/src/main/java/com/panda/fs/FileResource.java
@@ -0,0 +1,14 @@
+package com.panda.fs;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@PropertySource("classpath:file-${spring.profiles.active}.properties")
+@ConfigurationProperties(prefix = "file")
+public class FileResource {
+    private String host;
+}
--- news-backend-service-fs/src/main/java/com/panda/fs/controller/FileUploadController.java
+++ news-backend-service-fs/src/main/java/com/panda/fs/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.panda.fs.controller;
 
 import com.panda.api.controller.fs.FileUploadControllerApi;
+import com.panda.fs.FileResource;
 import com.panda.fs.service.UploadService;
 import com.panda.json.result.ResponseResult;
 import com.panda.json.result.ResponseStatusEnum;
@@ -14,9 +15,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController implements FileUploadControllerApi {
     final static Logger logger = LoggerFactory.getLogger(FileUploadController.class);
+
     @Autowired
     UploadService uploadService;
-    
+
+    @Autowired
+    FileResource fileResource;
+
     @Override
     public ResponseResult uploadAvatar(String userId, MultipartFile file) throws Exception {
         if (file == null || StringUtils.isBlank(file.getOriginalFilename())) {
@@ -32,6 +37,10 @@ public class FileUploadController implements FileUploadControllerApi {
         }
         String path = uploadService.uploadFile(file, suffix);
         logger.info("path: " + path);
-        return ResponseResult.ok(path);
+        if (StringUtils.isBlank(path)) {
+            return ResponseResult.errorCustom(ResponseStatusEnum.FILE_UPLOAD_FAILD);
+        }
+        String finalPath = fileResource.getHost() + path;
+        return ResponseResult.ok(finalPath);
     }
 }
