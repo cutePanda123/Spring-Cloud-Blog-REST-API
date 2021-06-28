b0f89dd40fc7878309be78c500db43959c590cd3
cutePanda123 <peterli1234@126.com>
2021-06-27 18:28:26 -0700

    fixed file service cors issue

--- news-backend-service-fs/src/main/java/com/panda/fs/Application.java
+++ news-backend-service-fs/src/main/java/com/panda/fs/Application.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan("com.panda")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
