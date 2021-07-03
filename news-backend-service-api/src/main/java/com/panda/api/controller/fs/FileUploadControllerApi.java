238399a979bd08a0ea52e7d9c267b7ef55ca0975
cutePanda123 <peterli1234@126.com>
2021-07-03 16:08:44 -0700

    integrate GridFS to spring

--- news-backend-service-api/src/main/java/com/panda/api/controller/fs/FileUploadControllerApi.java
+++ news-backend-service-api/src/main/java/com/panda/api/controller/fs/FileUploadControllerApi.java
@@ -1,12 +1,10 @@
 package com.panda.api.controller.fs;
 
 import com.panda.json.result.ResponseResult;
+import com.panda.pojo.bo.NewAdminBO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Api(value = "File upload controller", tags = {"files"})
@@ -18,4 +16,7 @@ public interface FileUploadControllerApi {
             @RequestParam String userId,
             MultipartFile file
     ) throws Exception;
+
+    @PostMapping("/uploadToGridFS")
+    public ResponseResult uploadAvatar(@RequestBody NewAdminBO bo) throws Exception;
 }
---
+++ news-backend-service-fs/src/main/java/com/panda/fs/config/GridFsConfig.java
@@ -0,0 +1,22 @@
+package com.panda.fs.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GridFsConfig {
+    @Value("${spring.data.mongodb.database}")
+    private String connectionUri;
+
+    @Bean
+    public GridFSBucket gridFSBucket(MongoClient mongoClient) {
+        MongoDatabase database = mongoClient.getDatabase(connectionUri);
+        GridFSBucket bucket = GridFSBuckets.create(database);
+        return bucket;
+    }
+}
