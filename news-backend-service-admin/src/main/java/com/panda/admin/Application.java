d486c0b8d7f06ec9ff1d17dc8365b540ef341021
cutePanda123 <peterli1234@126.com>
2021-07-03 21:35:15 -0700

    only keep mongo db config IOC in file service

--- news-backend-service-admin/src/main/java/com/panda/admin/Application.java
+++ news-backend-service-admin/src/main/java/com/panda/admin/Application.java
@@ -2,10 +2,11 @@ package com.panda.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 @MapperScan(basePackages = "com.panda.admin.mapper")
 @ComponentScan("com.panda")
 public class Application {
