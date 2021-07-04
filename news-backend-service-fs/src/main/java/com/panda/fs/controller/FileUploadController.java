ff00cdb390bc65deb884e22e62eba3b0c8b0c8e2
cutePanda123 <peterli1234@126.com>
2021-07-03 20:51:49 -0700

    implement GridFs upload endpoint

--- news-backend-service-fs/src/main/java/com/panda/fs/controller/FileUploadController.java
+++ news-backend-service-fs/src/main/java/com/panda/fs/controller/FileUploadController.java
@@ -1,16 +1,23 @@
 package com.panda.fs.controller;
 
+import com.mongodb.client.gridfs.GridFSBucket;
 import com.panda.api.controller.fs.FileUploadControllerApi;
 import com.panda.fs.resources.FileResource;
 import com.panda.fs.service.UploadService;
 import com.panda.json.result.ResponseResult;
 import com.panda.json.result.ResponseStatusEnum;
+import com.panda.pojo.bo.NewAdminBO;
 import org.apache.commons.lang3.StringUtils;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Decoder;
+
+
+import java.io.ByteArrayInputStream;
 
 @RestController
 public class FileUploadController implements FileUploadControllerApi {
@@ -21,6 +28,9 @@ public class FileUploadController implements FileUploadControllerApi {
 
     @Autowired
     FileResource fileResource;
+
+    @Autowired
+    GridFSBucket gridFSBucket;
 
     @Override
     public ResponseResult uploadAvatar(String userId, MultipartFile file) throws Exception {
@@ -42,5 +52,16 @@ public class FileUploadController implements FileUploadControllerApi {
         }
         String finalPath = fileResource.getHost() + path;
         return ResponseResult.ok(finalPath);
+    }
+
+    @Override
+    public ResponseResult uploadAvatar(NewAdminBO bo) throws Exception {
+        String file64 = bo.getImg64();
+        byte[] bytes = new BASE64Decoder().decodeBuffer(file64);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        ObjectId fileId = gridFSBucket.uploadFromStream(bo.getUsername() +".png", inputStream);
+        String fileIdStr = fileId.toHexString();
+        return ResponseResult.ok(fileIdStr);
+        //return ResponseResult.ok();
     }
 }
