f108f8c0335bbbc95d88b7a5498295da2cafe4db
cutePanda123 <peterli1234@126.com>
2021-07-06 21:04:12 -0700

    implement spring string to date converter to convert date string in the url to date object

---
+++ news-backend-service-api/src/main/java/com/panda/api/config/StringToDateConverterConfig.java
@@ -0,0 +1,58 @@
+package com.panda.api.config;
+
+import com.panda.exception.EncapsulatedException;
+import com.panda.json.result.ResponseStatusEnum;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * convert date string in a request url to java.util.Date
+ */
+@Configuration
+public class StringToDateConverterConfig implements Converter<String, Date> {
+
+    private static final List<String> formatterList = new ArrayList<>(4);
+    static{
+        formatterList.add("yyyy-MM");
+        formatterList.add("yyyy-MM-dd");
+        formatterList.add("yyyy-MM-dd hh:mm");
+        formatterList.add("yyyy-MM-dd hh:mm:ss");
+    }
+
+    @Override
+    public Date convert(String source) {
+        String value = source.trim();
+        if ("".equals(value)) {
+            return null;
+        }
+        if(source.matches("^\\d{4}-\\d{1,2}$")){
+            return parseDate(source, formatterList.get(0));
+        }else if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2}$")){
+            return parseDate(source, formatterList.get(1));
+        }else if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2} {1}\\d{1,2}:\\d{1,2}$")){
+            return parseDate(source, formatterList.get(2));
+        }else if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2} {1}\\d{1,2}:\\d{1,2}:\\d{1,2}$")){
+            return parseDate(source, formatterList.get(3));
+        }else {
+            EncapsulatedException.display(ResponseStatusEnum.SYSTEM_DATE_PARSER_ERROR);
+        }
+        return null;
+    }
+    
+    public Date parseDate(String dateStr, String formatter) {
+        Date date = null;
+        try {
+            DateFormat dateFormat = new SimpleDateFormat(formatter);
+            date = dateFormat.parse(dateStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+}
