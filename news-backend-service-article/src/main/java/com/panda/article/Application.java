7741f84742ea80195dc10777a3446bfc08787cc7
cutePanda123 <peterli1234@126.com>
2021-08-02 11:33:57 -0700

    apply ribbon client side random load balance rule

--- news-backend-service-article/src/main/java/com/panda/article/Application.java
+++ news-backend-service-article/src/main/java/com/panda/article/Application.java
@@ -1,9 +1,11 @@
 package com.panda.article;
 
+import com.ribbon.rule.CustomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -11,6 +13,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.panda.article.mapper")
 @ComponentScan("com.panda")
 @EnableEurekaClient
+@RibbonClient(name = "service-user", configuration = CustomRule.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
