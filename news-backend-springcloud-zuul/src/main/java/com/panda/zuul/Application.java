469818189198da9fe58320afc7719946e7388630
cutePanda123 <peterli1234@126.com>
2021-08-09 21:10:13 -0700

    replace service url with service id from eureka in zuul config

--- news-backend-springcloud-zuul/src/main/java/com/panda/zuul/Application.java
+++ news-backend-springcloud-zuul/src/main/java/com/panda/zuul/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication(exclude = {
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
         MongoAutoConfiguration.class
 })
 @EnableZuulProxy
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
