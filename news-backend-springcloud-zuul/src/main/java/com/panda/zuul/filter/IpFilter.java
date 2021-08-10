5e763626a6571f316773c5d74089051f998af12f
cutePanda123 <peterli1234@126.com>
2021-08-09 22:51:24 -0700

    created a custom zuul ip filter

---
+++ news-backend-springcloud-zuul/src/main/java/com/panda/zuul/filter/IpFilter.java
@@ -0,0 +1,28 @@
+package com.panda.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IpFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
