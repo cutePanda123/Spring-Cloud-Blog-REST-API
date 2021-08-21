a44a0761a5b263e8819049a5c07c6455028c952d
cutePanda123 <peterli1234@126.com>
2021-08-21 04:44:19 -0700

    implemented a message api test endpoint

--- news-backend-service-article/src/main/java/com/panda/article/controller/HealthCheckController.java
+++ news-backend-service-article/src/main/java/com/panda/article/controller/HealthCheckController.java
@@ -1,10 +1,12 @@
 package com.panda.article.controller;
 
+import com.panda.article.stream.StreamService;
 import com.panda.json.result.ResponseResult;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/service-article/healthcheck")
 public class HealthCheckController {
+    @Autowired
+    private StreamService streamService;
+
     @ApiOperation(value = "server health check api", notes = "server health check api", httpMethod = "GET")
     @GetMapping("/checkwebserver")
     public Object checkWebServer() {
         log.info("health check request received.");
         return ResponseResult.ok("Health check passed.");
+    }
+
+    @ApiOperation(value = "test cloud stream message api", notes = "test cloud stream message api", httpMethod = "GET")
+    @GetMapping("/testmessageapi")
+    public Object testCloudStream() {
+        log.info("test cloud stream message api request received.");
+        streamService.sendMsg();
+        return ResponseResult.ok("cloud stream message api passed.");
     }
 }
